1c7fa9fe1817682fcddb5bd345057dbc05d33239
mbc <sub07007@gmail.com>
2026-09-22 11:24:29 +0900

    WaveSpawner.TotalRounds/MaxAliveMonsters가 config가 null일 때 크래시하던 근본 원인 수정
    지난 GameHud 방어 코드(WaveSpawner 참조 자체가 null인 경우)로는 못 잡은
    케이스였다 - WaveSpawner 컴포넌트 자체는 살아있는데 그 내부의 config
    필드만 null이라 TotalRounds/MaxAliveMonsters 프로퍼티에서 곧바로
    NullReferenceException이 났다. 두 프로퍼티 모두 config가 null이면
    0을 반환하도록 방어했다.

--- Assets/Scripts/Wave/WaveSpawner.cs
+++ Assets/Scripts/Wave/WaveSpawner.cs
@@ -22,8 +22,8 @@ namespace MRD.Wave
 
         public int CurrentRound { get; private set; }
         public int AliveMonsterCount => _activeMonsters.Count;
-        public int TotalRounds => config.totalRounds;
-        public int MaxAliveMonsters => config.maxAliveMonsters;
+        public int TotalRounds => config != null ? config.totalRounds : 0;
+        public int MaxAliveMonsters => config != null ? config.maxAliveMonsters : 0;
         public bool IsGameOver { get; private set; }
         public bool IsAllRoundsCleared { get; private set; }
 
